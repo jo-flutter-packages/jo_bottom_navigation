42020a71932f6acccad2519e4529461654281638
jalal <jalal@DESKTOP-MSA89D1>
2023-06-22 18:29:25 +0330

    dsfd

--- lib/jo_bottom_navigation.dart
+++ lib/jo_bottom_navigation.dart
@@ -1,7 +1,56 @@
 library jo_bottom_navigation;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'jo_navigation_item.dart';
+
+String? joNavigationCurrentRoute;
+
+class JONavigation extends StatefulWidget {
+  const JONavigation({
+    Key? key,
+    this.rtl = true,
+    required this.items,
+  }) : super(key: key);
+  final bool rtl;
+  final List<JONavigationItem> items;
+
+  @override
+  State<JONavigation> createState() => _JONavigationState();
+}
+
+class _JONavigationState extends State<JONavigation> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: widget.rtl ? TextDirection.rtl : TextDirection.ltr,
+      child: Container(
+        height: 70.0,
+        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+        decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.8),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+              ),
+            ]),
+        child: ListView(
+          padding: const EdgeInsets.all(0.0),
+          scrollDirection: Axis.horizontal,
+          children: widget.items,
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/jo_navigation_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:jo_bottom_navigation/jo_bottom_navigation.dart';
+
+class JONavigationItem extends StatelessWidget {
+  const JONavigationItem({
+    Key? key,
+    required this.icon,
+    required this.route,
+    required this.callback,
+    this.visible,
+    this.enable,
+  }) : super(key: key);
+  final IconData icon;
+  final String route;
+  final Function() callback;
+  final bool? visible;
+  final bool? enable;
+
+  @override
+  Widget build(BuildContext context) {
+    return visible == true
+        ? Container(
+            height: 60.0,
+            width: 65.0,
+            decoration: BoxDecoration(
+              color: joNavigationCurrentRoute != route
+                  ? null
+                  : Theme.of(context).cardColor,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            margin: const EdgeInsets.all(10.0),
+            child: IconButton(
+              enableFeedback: enable,
+              padding: const EdgeInsets.all(0.0),
+              icon: Icon(
+                icon,
+                color: joNavigationCurrentRoute == route
+                    ? null
+                    : visible == false || enable == false
+                        ? Theme.of(context).disabledColor
+                        : Theme.of(context).cardColor,
+              ),
+              iconSize: 30.0,
+              onPressed: visible == false || enable == false
+                  ? null
+                  : () {
+                      joNavigationCurrentRoute = route;
+                      callback.call();
+                      Navigator.of(context).pushNamed(route);
+                    },
+            ),
+          )
+        : const SizedBox();
+  }
+}
